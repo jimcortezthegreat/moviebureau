aaf8daf66e5e5983bb4bf4b66419bbd9faefc243
chad <chadbsantiago@gmail.com>
2022-06-15 17:13:37 +0800

    main

---
+++ _next/static/ZErJLuUtSPYW5VROG86t1/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fanime\u002F[id]","\u002Ftv-news\u002F[id]","\u002Fmovie-news\u002F[id]","\u002Farticle\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
